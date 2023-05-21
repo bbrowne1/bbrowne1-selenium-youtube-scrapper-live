5cd97e20a955519d6a494ca0b0e72fde58820eea
bbrowne1 <bayobrowne@hotmail.com>
2023-05-21 20:51:18 +0000

    changed repo name

--- scraper.py
+++ scraper.py
@@ -1,1 +1,8 @@
-print ('Hello World')
+import requests
+
+YOUTUBE_TRENDING_URL = 'https://www.youtube.com/feed/trending?bp=6gQJRkVleHBsb3Jl'
+
+response = requests.get(YOUTUBE_TRENDING_URL)
+print ('Status Code', response.status_code)
+with open ('trending.html', 'w') as f:
+           f.write(response.text)
