cb92ef3dac4a5f1d0240512b65de148badea5021
bbrowne1 <bayobrowne@hotmail.com>
2023-05-31 20:52:24 +0000

    title , url, thumbnail_url, channel,  description

--- scraper.py
+++ scraper.py
@@ -1,5 +1,6 @@
 from selenium import webdriver 
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
 
 
 YOUTUBE_TRENDING_URL = 'https://www.youtube.com/feed/trending?bp=6gQJRkVleHBsb3Jl'
@@ -9,16 +10,44 @@ def get_driver():
   chrome_options.add_argument('--no-sandbox')
   chrome_options.add_argument('--headless')
   chrome_options.add_argument('--disable-dev-shm-usage')
-  driver =webdriver.Chrome(options=chrome_options)
+  driver = webdriver.Chrome(options=chrome_options)
   return driver
+
+
+def get_videos(driver):
+  VIDEO_DIV_TAG = 'ytd-video-renderer'
+  driver.get(YOUTUBE_TRENDING_URL)
+  videos= driver.find_elements(By.TAG_NAME, VIDEO_DIV_TAG)
+  return videos
 
 if __name__ == "__main__":
   print ('Creating driver')
   driver = get_driver()
 
-print('Fetching the page')
-driver.get(YOUTUBE_TRENDING_URL)
-print ('Get the video divs')
-driver.find_elements_by_class_name(VIDEO_DIV_CLASS)
+print('Fetching trending videos')
+videos = get_videos(driver)
 
-print('Page title:', driver.title)
+print (f'Found {len(videos)} videos')
+
+
+#title , url, thumbnail_url, channel, viewsm uploaded, description
+
+video = videos[0]
+
+title_tag = video.find_element(By.ID, 'video-title')
+title =title_tag.text
+url = title_tag.get_attribute('href')
+
+thumbnail_tag= video.find_element(By.TAG_NAME, 'img')
+thumbnail_url= thumbnail_tag.get_attribute('src')
+
+#channel_div = video.find_element(By.CLASS_NAME, 'ytd-channel-name')
+#channel_name= channel_div.text
+
+description = video.find_element(By.ID, 'description-text').text
+
+print ('Title', title)
+print ('URL:', url)
+print ('Thumbnail url:', thumbnail_url)
+#print ('Channel Name:', channel_name)
+print ('Description:', description)
