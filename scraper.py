8aef61a82d1fb42590ad02433884c71643b2f26c
bbrowne1 <bayobrowne@hotmail.com>
2023-05-31 22:02:02 +0000

    pandas

--- scraper.py
+++ scraper.py
@@ -1,9 +1,10 @@
+import pandas as pd
 from selenium import webdriver 
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 
 
-YOUTUBE_TRENDING_URL = 'https://www.youtube.com/feed/trending?bp=6gQJRkVleHBsb3Jl'
+YOUTUBE_TRENDING_URL = 'https://www.youtube.com/feed/trending'
 
 def get_driver():
   chrome_options = Options()
@@ -32,8 +33,7 @@ def parse_video(video):
 #channel_name= channel_div.text
 
   description = video.find_element(By.ID, 'description-text').text
-
-return {
+  return {
    'title': title,
     'url': url,
     'thumbnail_url': thumbnail_url,
@@ -51,14 +51,17 @@ videos = get_videos(driver)
 
 print (f'Found {len(videos)} videos')
 
-print ('Parsing top ten videos')
+print ('Parsing top 10 videos')
 videos_data = [parse_video(video) for video in videos[:10]]
 
-print (videos_data)
+print ('Save the data to a CSV')
+videos_df = pd.DataFrame(videos_data)
+print (videos_df)
+videos_df.to.csv('trending.csv')
 
 #title , url, thumbnail_url, channel, viewsm uploaded, description
 
-video = videos[0]
+#video = videos[0]
 
 
 #print ('Title', title)
