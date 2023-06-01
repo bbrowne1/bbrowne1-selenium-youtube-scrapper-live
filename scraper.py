c20bc3b47126dce2c42b9a4ff93080dfa95276b5
bbrowne1 <bayobrowne@hotmail.com>
2023-06-01 17:57:50 +0000

    Top ten trending video -music

--- scraper.py
+++ scraper.py
@@ -2,9 +2,13 @@ import pandas as pd
 from selenium import webdriver 
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
+import time
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
 
 
-YOUTUBE_TRENDING_URL = 'https://www.youtube.com/feed/trending'
+
+YOUTUBE_TRENDING_URL = 'https://www.youtube.com/feed/trending?bp=4gINGgt5dG1hX2NoYXJ0cw%3D%3D'
 
 def get_driver():
   chrome_options = Options()
@@ -12,6 +16,7 @@ def get_driver():
   chrome_options.add_argument('--headless')
   chrome_options.add_argument('--disable-dev-shm-usage')
   driver = webdriver.Chrome(options=chrome_options)
+  driver.implicitly_wait(10) # seconds
   return driver
 
 
@@ -22,17 +27,18 @@ def get_videos(driver):
   return videos
 
 def parse_video(video):
+  
   title_tag = video.find_element(By.ID, 'video-title')
   title =title_tag.text
   url = title_tag.get_attribute('href')
-
-  thumbnail_tag=    video.find_element(By.TAG_NAME, 'img')
-  thumbnail_url= thumbnail_tag.get_attribute('src')
-
-  #channel_div = video.find_element(By.CLASS_NAME, 'ytd-channel-name')
-#channel_name= channel_div.text
-
+  
+  thumbnail_tag =    video.find_element(By.TAG_NAME, 'img')
+  thumbnail_url = thumbnail_tag.get_attribute('src')
+ 
+  channel_div = video.find_element(By.CLASS_NAME,'ytd-channel-name')
+  channel_name = channel_div.text
   description = video.find_element(By.ID, 'description-text').text
+ 
   return {
    'title': title,
     'url': url,
@@ -57,7 +63,11 @@ videos_data = [parse_video(video) for video in videos[:10]]
 print ('Save the data to a CSV')
 videos_df = pd.DataFrame(videos_data)
 print (videos_df)
-videos_df.to.csv('trending.csv')
+videos_df.to_csv('trending.csv', index =None)
+
+
+
+
 
 #title , url, thumbnail_url, channel, viewsm uploaded, description
 
